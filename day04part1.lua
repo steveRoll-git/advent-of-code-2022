416ec7ac4e52f0fd948bd955c027c7a238d50168
Ron Tseytlin <ronts390@gmail.com>
2022-12-04 12:12:44 +0200

    finished day 4 part 1

---
+++ day04part1.lua
@@ -0,0 +1,16 @@
+local input = io.open("input04.txt", "r")
+
+local count = 0
+
+for l in input:lines("*l") do
+  local a1, b1, a2, b2 = l:match("(%d*)-(%d*),(%d*)-(%d*)")
+  a1 = tonumber(a1)
+  a2 = tonumber(a2)
+  b1 = tonumber(b1)
+  b2 = tonumber(b2)
+  if (a1 >= a2 and b1 <= b2) or (a2 >= a1 and b2 <= b1) then
+    count = count + 1
+  end
+end
+
+print(count)
