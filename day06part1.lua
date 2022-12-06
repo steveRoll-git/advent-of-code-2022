31e3707e6cc7953876e56145fbe46712930af44c
Ron Tseytlin <ronts390@gmail.com>
2022-12-06 08:22:32 +0200

    finished day 6

---
+++ day06part1.lua
@@ -0,0 +1,22 @@
+local function allDifferent(str)
+	local seen = {}
+	for i=1, #str do
+		local c = str:sub(i, i)
+		if seen[c] then
+			return false
+		end
+		seen[c] = true
+	end
+	return true
+end
+
+local input = io.open("input06.txt", "r"):read("*a")
+
+local back = 4
+
+for i = 1, #input - back do
+	if allDifferent(input:sub(i, i + back - 1)) then
+		print(i + back - 1)
+		break
+	end
+end
