5eee6e31c48e2abc59c2fbd4eecc9c591ff02e2a
Ron Tseytlin <ronts390@gmail.com>
2022-12-07 10:58:41 +0200

    finished day 7 part 1

---
+++ day07part1.lua
@@ -0,0 +1,55 @@
+local input = io.open("input07.txt", "r")
+
+local lines = input:lines("*l")
+
+local line
+
+local function next()
+  line = lines()
+end
+
+next()
+
+local currentDir = "/"
+
+local sizes = {["/"] = 0}
+
+while line ~= nil do
+  if line:sub(1,1) == "$" then
+    local command, param = line:match("%$ (..) ?(.*)")
+    if command == "cd" then
+      if param == "/" then
+        currentDir = "/"
+      elseif param == ".." then
+        currentDir = currentDir:match("(.*/).-/$")
+      else
+        currentDir = ("%s%s/"):format(currentDir, param)
+        sizes[currentDir] = sizes[currentDir] or 0
+      end
+      next()
+    elseif command == "ls" then
+      next()
+      while line ~= nil and line:sub(1,1) ~= "$" do
+        local size, name = line:match("(.*) (.*)")
+        if size ~= "dir" then
+          local dir = currentDir
+          repeat
+            sizes[dir] = sizes[dir] + tonumber(size)
+            dir = dir:match("(.*/).-/$")
+          until dir == nil
+        end
+        next()
+      end
+    end
+  end
+end
+
+local sum = 0
+
+for _, size in pairs(sizes) do
+  if size <= 100000 then
+    sum = sum + size
+  end
+end
+
+print(sum)
